ef254f6b125e866100e5771d4ab3bf5157b3ba60
Lucai994 <luca.muratori.94@hotmail.it>
2021-12-04 14:27:18 +0100

    exercise 4 solved

--- example.js
+++ example.js
@@ -1,8 +1,14 @@
-/* EXTRA 3
- Write a piece of code for getting the 
- minimum numerical value from an array.
-*/
+//EXTRA 4
+//Write a piece of code for getting only 
+//even numerical values from an array.
 
-let a=[ 6, 89 ]
 
-console.log(Math.min(...a));
+let a=[1, 2, 4, 5, 7, 8, 9, 11, 12]
+let even=[]
+
+for(i=0; i<a.length; i++){
+    if(a[i]%2===0){
+        even.push(a[i]);
+    }
+}
+console.log(even)
--- excercised4-extra.js
+++ excercised4-extra.js
@@ -35,7 +35,17 @@ console.log(Math.min(...a));
  Write a piece of code for getting only even numerical values from an array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+/*
+let a=[1, 2, 4, 5, 7, 8, 9, 11, 12]
+let even=[]
+
+for(i=0; i<a.length; i++){
+    if(a[i]%2===0){
+        even.push(a[i]);
+    }
+}
+console.log(even)
+*/
 
 /* EXTRA 5
  Write a piece of code for deleting only even entries from an array.
